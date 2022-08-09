e8ff26b36ac1be756ad0df75d8a8b0c76bf30aee
Shalomtash <99330267+shalomtash@users.noreply.github.com>
2022-08-09 17:19:09 +0300

    Update review.py

--- models/review.py
+++ models/review.py
@@ -1,12 +1,16 @@
 #!/usr/bin/python3
-"""
-Defines review class
-"""
+"""Defines the Review class."""
 from models.base_model import BaseModel
 
 
 class Review(BaseModel):
-    """Reviews made by users about a place"""
+    """Represent a review.
+    Attributes:
+        place_id (str): The Place id.
+        user_id (str): The User id.
+        text (str): The text of the review.
+    """
+
     place_id = ""
     user_id = ""
     text = ""
